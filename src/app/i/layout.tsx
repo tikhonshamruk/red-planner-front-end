274f9ea9cb892c281928d0c005d2ab312260d3eb
tikhon <tikhonshamruk@gmail.com>
2025-08-03 20:00:04 +0300

    understand how working children props

--- src/app/i/layout.tsx
+++ src/app/i/layout.tsx
@@ -1,5 +1,9 @@
-import { DashboardLayout } from "@/components/dashboards/DashboardLayout";
+import { DashboardLayout } from "@/components/dashboards-layout/DashboardLayout";
+import { LayoutProps } from "../../../.next/types/app/layout";
+import { PropsWithChildren } from "react";
 
-export default function Layout(){
-    return <DashboardLayout></DashboardLayout>
+export default function Layout({children}: PropsWithChildren<unknown>){
+    return (<DashboardLayout>
+        {children}
+    </DashboardLayout>)
 }
---
+++ src/components/dashboards-layout/DashboardLayout.tsx
@@ -0,0 +1,7 @@
+import { PropsWithChildren } from "react";
+
+export function DashboardLayout({children}:PropsWithChildren){
+    return <div>DashboardLayout
+        {children}
+    </div>
+}
