77a3d2581027780e6eb2c22dce7ab6cabff935ce
tikhon <tikhonshamruk@gmail.com>
2025-08-07 14:07:59 +0300

    maybe done filter

--- src/app/i/tasks/columns.data.ts
+++ src/app/i/tasks/columns.data.ts
@@ -1,10 +1,17 @@
-import dayjs , {type Dayjs} from 'dayjs'
+import dayjs, { type Dayjs } from 'dayjs'
 import 'dayjs/locale/ru'
 //подключает локали, если она вам нужна
 
 import isoWeek from 'dayjs/plugin/isoWeek'
-
 import weekOfYear from 'dayjs/plugin/weekOfYear'
 
 dayjs.extend(weekOfYear)
 dayjs.extend(isoWeek)
+
+export const FILTERS: Record<string, Dayjs> = {
+	today: dayjs().startOf('day'),
+	tomorrow: dayjs().add(1, 'day').startOf('day'),
+	'on-this-week': dayjs().endOf('isoWeek'),
+	'on-next-week': dayjs().add(1, 'week').startOf('day'),
+	later: dayjs().add(2, 'week').startOf('day')
+}
