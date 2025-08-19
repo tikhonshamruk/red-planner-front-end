74d4cf3f22a8a048d01844f240054c747c25c7ac
tikhon <tikhonshamruk@gmail.com>
2025-08-19 08:19:09 +0300

    making select component

--- src/components/ui/SingleSelect.tsx
+++ src/components/ui/SingleSelect.tsx
@@ -1,37 +1,36 @@
-import FormControl from '@mui/material/FormControl';
-import InputLabel from '@mui/material/InputLabel';
-import Select from '@mui/material/Select';
-import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import Select from '@mui/material/Select'
 
 export interface IOption {
 	label: string
 	value: string
 }
 
-interface IDateSelect{
-  data: IOption[]
-  onChange: (value: string) => void
-  value: string | undefined
+interface IDateSelect {
+	data: IOption[]
+	onChange: (value: string) => void
+	value: string | undefined
 }
 
-export function SingleSelect({data,onChange,value}: IDateSelect) {
-  return (
-    <div>
-      <FormControl fullWidth>
-  <InputLabel id="demo-simple-select-label">Age</InputLabel>
-  <Select
-    labelId="demo-simple-select-label"
-    id="demo-simple-select"
-    value={value}
-    label="Age"
-    onChange={(e) => onChange(e.target.value as string)}
-  >{
-    data.map(item =>(
-      <MenuItem value={item.value}>{item.label}</MenuItem>
-    ))
-  }
-  </Select>
-</FormControl>
-    </div>
-  )
+export function SingleSelect({ data, onChange, value }: IDateSelect) {
+	return (
+		<div>
+			<FormControl fullWidth>
+				<InputLabel id='demo-simple-select-label'>Age</InputLabel>
+				<Select
+					labelId='demo-simple-select-label'
+					id='demo-simple-select'
+					value={value}
+					label='Priority'
+					onChange={e => onChange(e.target.value as string)}
+				>
+					{data.map(item => (
+						<MenuItem value={item.value}>{item.label}</MenuItem>
+					))}
+				</Select>
+			</FormControl>
+		</div>
+	)
 }
